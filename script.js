32bd71e874618f39a745b2492916507c2949e15f
juny62017 <juny62017@gmail.com>
2026-05-16 23:22:00 +0530

    Addeed temperature themes and finalize TempConvert

--- script.js
+++ script.js
@@ -4,4 +4,190 @@ let convertBtn =
 let resetBtn =
     document.querySelector(".reset-btn");
 
-console.log("TempConvert Started");
+let temperatureInput =
+    document.getElementById("temperatureInput");
+
+let fromUnit =
+    document.getElementById("fromUnit");
+
+let toUnit =
+    document.getElementById("toUnit");
+
+let resultNumber =
+    document.querySelector(".result-number");
+
+let resultText =
+    document.querySelector(".result-text");
+
+convertBtn.onclick = function () {
+
+    let inputValue =
+        temperatureInput.value;
+
+    if (inputValue === "") {
+
+        resultNumber.innerText =
+            "00.0";
+
+        resultText.innerText =
+            "Please enter a value";
+
+        resultText.style.color =
+            "#d67b7b";
+
+        return;
+    }
+
+    let value =
+        Number(inputValue);
+
+    let convertedValue =
+        value;
+
+    if (
+        fromUnit.value === "celsius" &&
+        toUnit.value === "fahrenheit"
+    ) {
+
+        convertedValue =
+            (value * 9/5) + 32;
+
+    }
+
+    else if (
+        fromUnit.value === "celsius" &&
+        toUnit.value === "kelvin"
+    ) {
+
+        convertedValue =
+            value + 273.15;
+
+    }
+
+    else if (
+        fromUnit.value === "fahrenheit" &&
+        toUnit.value === "celsius"
+    ) {
+
+        convertedValue =
+            (value - 32) * 5/9;
+
+    }
+
+    else if (
+        fromUnit.value === "fahrenheit" &&
+        toUnit.value === "kelvin"
+    ) {
+
+        convertedValue =
+            ((value - 32) * 5/9)
+            + 273.15;
+
+    }
+
+    else if (
+        fromUnit.value === "kelvin" &&
+        toUnit.value === "celsius"
+    ) {
+
+        convertedValue =
+            value - 273.15;
+
+    }
+
+    else if (
+        fromUnit.value === "kelvin" &&
+        toUnit.value === "fahrenheit"
+    ) {
+
+        convertedValue =
+            ((value - 273.15)
+            * 9/5) + 32;
+
+    }
+
+    resultNumber.innerText =
+        convertedValue.toFixed(1);
+
+    document.body.classList.remove(
+        "cold-theme",
+        "normal-theme",
+        "hot-theme"
+    );
+
+    if (convertedValue < 15) {
+
+        document.body.classList.add(
+            "cold-theme"
+        );
+
+        resultText.innerText =
+            "Cold Temperature";
+
+        resultText.style.color =
+            "#5b8db8";
+
+    }
+
+    else if (
+        convertedValue >= 15 &&
+        convertedValue <= 30
+    ) {
+
+        document.body.classList.add(
+            "normal-theme"
+        );
+
+        resultText.innerText =
+            "Normal Temperature";
+
+        resultText.style.color =
+            "#7da67d";
+
+    }
+
+    else {
+
+        document.body.classList.add(
+            "hot-theme"
+        );
+
+        resultText.innerText =
+            "Hot Temperature";
+
+        resultText.style.color =
+            "#d67b7b";
+
+    }
+
+};
+
+resetBtn.onclick = function () {
+
+    temperatureInput.value = "";
+
+    fromUnit.value =
+        "celsius";
+
+    toUnit.value =
+        "fahrenheit";
+
+    resultNumber.innerText =
+        "00.0";
+
+    resultText.innerText =
+        "Normal Temperature";
+
+    resultText.style.color =
+        "#7da67d";
+
+    document.body.classList.remove(
+        "cold-theme",
+        "hot-theme"
+    );
+
+    document.body.classList.add(
+        "normal-theme"
+    );
+
+};
